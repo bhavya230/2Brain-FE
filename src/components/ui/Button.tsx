ff8020e1b28ecb75ff0682e7b705453e3b6df7d2
unknown <raghavbhavyasingh@gmail.com>
2025-09-15 19:41:47 +0530

    button component craeted

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,20 +1,32 @@
+import type { ReactElement } from "react";
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
-
 export interface ButtonProps{
     variant:"primary"|"secondary";
     size:"md"|"sm"|"lg";
     text:string;
     onclick:()=>void;
-    startIcon?:any;
-    endIcon?:any;
+    startIcon?:ReactElement;
+    endIcon?:ReactElement;
 }
 
+const variantStyles={
+    'primary':'bg-purple-600 text-white',
+    'secondary':'bg-white text-purple-300'
+}
+
+const defaultStyles="rounded-md flex";
+
+const sizeStyles={
+    "sm":"py-1 px-2",
+    "md":" py-2 px-4",
+    "lg":" py-3 px-6"
+}
 export const Button=(props:ButtonProps)=>{
    
     return (
-        <div>
-            <button>{props.text}</button>
-        </div>
+        <button className={`${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>
+           {props.startIcon ?<div className="pr-2">{props.startIcon}</div>:null} {props.text} {props.endIcon?<div>{props.endIcon}</div>:null}
+        </button>
     )
 }
